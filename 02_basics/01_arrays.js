03520d549256810ab53596f9aed63c6f57aa9d82
Prerana Singh <singhprerana199@gmail.com>
2026-01-07 15:15:31 +0530

    Completed arrays part 1 in java script

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+const myArr= [10, 20, 30, 40, 50];
+
+const myArr2 = new Array(10, 20, 30, 40, 50);
+//console.log(myArr[1]);
+ 
+
+// ARRAY METHODS 
+
+//myArr.push(60); 
+//myArr.pop();
+//myArr.shift();
+//myArr.unshift(5);
+
+
+//console.log(myArr.includes(50));
+//console.log(myArr.indexOf(30));
+
+
+//const newArr = myArr.join()
+
+//console.log(myArr);
+//console.log(typeof newArr);
+
+//slice , splice
+
+console.log("A")
