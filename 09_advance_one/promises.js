10111f6e50bbc443cac42423f17b7584eca1d16b
Prerana Singh <singhprerana199@gmail.com>
2026-01-19 22:52:20 +0530

    promides

---
+++ 09_advance_one/promises.js
@@ -0,0 +1,56 @@
+const promiseOne = new Promise(function(resolve,reject){
+    //Do an async task
+    //DB calls, cryptography,network
+    setTimeout(function() {
+        console.log('Async task is complete');
+        resolve()
+        
+    },1000)
+})
+
+promiseOne.then(function(){ 
+    console.log("Promise consumed");
+})
+
+new Promise(function(resolve,reject){
+    setTimeout(function() {
+        console.log('Async task 2');
+        resolve() 
+     },1000)
+}).then(function(){ 
+    console.log("Async task 2 reolved");
+})
+
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function() {
+        resolve({username: "shruti",email:"shruti@gmail.com"})
+    }, 1000)
+})
+
+promiseThree.then(function(user){
+    console.log(user);
+})
+
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true 
+        if(!error) {
+            resolve({username:"sh" , password:"123"})
+        }else{
+            reject('ERROR: Something went wrong')
+        }
+    },1000)
+})
+
+promiseFour
+.then(() => {
+    console.log(user);
+    return user.username
+}).then((username) => {
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(() => console.log("the promise is either resolved or rejected"))
+    
