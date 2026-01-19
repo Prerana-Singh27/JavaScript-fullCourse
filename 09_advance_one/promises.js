1538aa9d91078dbe864ad73206de39da2e168abf
Prerana Singh <singhprerana199@gmail.com>
2026-01-19 23:24:00 +0530

    promises

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -54,3 +54,51 @@ promiseFour
     console.log(error);
 }).finally(() => console.log("the promise is either resolved or rejected"))
     
+
+
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(function(){
+        let error = true 
+        if(!error) {
+            resolve({username:"javascript" , password:"123"})
+        }else{
+            reject('ERROR: js went wrong')
+        }
+    },1000)
+});
+
+async function consumePromiseFive(){
+    try {
+         const response = await promiseFive
+        console.log(response);
+    } catch (error) {
+        console.log(error);
+    }
+   
+}
+
+consumePromiseFive()
+
+
+// async function getAllUsers(){
+//     try {
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+//          const data = await response.json()
+//         console.log(data);
+//     } catch (error) {
+//         console.log("E: ", error);
+//     }
+//  }
+
+//getAllUsers()
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response) => {
+    return response.json()
+})
+.then((data) => {
+    console.log(data);
+})
+.catch((error) => console.log(error))
